2203a144379bd126292b649eb6cc81560785ba56
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-11-13 10:11:26 -0300

    Criando todos os aquivos para acesso a dados da api

---
+++ ApiSGCOlimpiada/Data/ProdutoPedidoOrcamentoDAO/IProdutoPedidoOrcamentoDAO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiSGCOlimpiada.Data.ProdutoPedidoOrcamentoDAO
+{
+    public interface IProdutoPedidoOrcamentoDAO
+    {
+    }
+}
