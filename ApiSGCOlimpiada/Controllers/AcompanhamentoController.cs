e8084b6921a8b454afd1c24ae27a88fe9a733912
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-12-03 16:16:25 -0300

    Implementação dos end points para acompanhamento e status

---
+++ ApiSGCOlimpiada/Controllers/AcompanhamentoController.cs
@@ -0,0 +1,64 @@
+﻿using ApiSGCOlimpiada.Data.AcompanhamentoDAO;
+using ApiSGCOlimpiada.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiSGCOlimpiada.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AcompanhamentoController : ControllerBase
+    {
+        private readonly IAcompanhamentoDAO dao;
+
+        public AcompanhamentoController(IAcompanhamentoDAO dao)
+        {
+            this.dao = dao;
+        }
+        [HttpGet]
+        public IEnumerable<Acompanhamento> GetAll()
+        {
+            return dao.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetAcompanhamento")]
+        public IActionResult GetAcompanhamentoById(long id)
+        {
+            var acompanhamento = dao.Find(id);
+            if (acompanhamento == null)
+                return NotFound(new { Message = "Acompanhamento não encontrada" });
+            return new ObjectResult(acompanhamento);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Acompanhamento acompanhamento)
+        {
+            if (string.IsNullOrEmpty(acompanhamento.Observacao) || string.IsNullOrEmpty(acompanhamento.Date.ToString()) || 
+                acompanhamento.StatusId == 0 || acompanhamento.UsuarioId == 0 || acompanhamento.SolicitacaoCompraId == 0)
+                return BadRequest(new { Message = "Todos os campos são obrigatórios" });
+
+            if (dao.Add(acompanhamento))
+                return CreatedAtRoute("GetAcompanhamento", new { id = acompanhamento.Id }, acompanhamento);
+            return BadRequest(new { Message = "Erro interno no servidor" });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Acompanhamento acompanhamento, long id)
+        {
+            if (string.IsNullOrEmpty(acompanhamento.Observacao) || string.IsNullOrEmpty(acompanhamento.Date.ToString()) ||
+                 acompanhamento.StatusId == 0 || acompanhamento.UsuarioId == 0 || acompanhamento.SolicitacaoCompraId == 0)
+                return BadRequest(new { Message = "Todos os campos são obrigatórios" });
+            if (dao.Find(id) == null)
+                return NotFound(new { Message = "Acompanhamento não encontrada" });
+
+            if (dao.Update(acompanhamento, id))
+                return CreatedAtRoute("GetAcompanhamento", new { id = acompanhamento.Id }, acompanhamento);
+            return BadRequest(new { Message = "Erro interno no servidor" });
+        }
+    }
+}
+
---
+++ ApiSGCOlimpiada/Controllers/StatusController.cs
@@ -0,0 +1,61 @@
+﻿using ApiSGCOlimpiada.Data.StatusDAO;
+using ApiSGCOlimpiada.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiSGCOlimpiada.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly IStatusDAO dao;
+
+        public StatusController(IStatusDAO dao)
+        {
+            this.dao = dao;
+        }
+        [HttpGet]
+        public IEnumerable<Status> GetAll()
+        {
+            return dao.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetStatus")]
+        public IActionResult GetStatusById(long id)
+        {
+            var status = dao.Find(id);
+            if (status == null)
+                return NotFound(new { Message = "Ocupação não encontrada" });
+            return new ObjectResult(status);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Status status)
+        {
+            if (string.IsNullOrEmpty(status.Descricao))
+                return BadRequest(new { Message = "Todos os campos são obrigatórios" });
+
+            if (dao.Add(status))
+                return CreatedAtRoute("GetStatus", new { id = status.Id }, status);
+            return BadRequest(new { Message = "Erro interno no servidor" });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Status status, long id)
+        {
+            if (string.IsNullOrEmpty(status.Descricao))
+                return BadRequest(new { Message = "Todos os campos são obrigatórios" });
+            if (dao.Find(id) == null)
+                return NotFound(new { Message = "Ocupação não encontrada" });
+            if (dao.Update(status, id))
+                return CreatedAtRoute("GetStatus", new { id = status.Id }, status);
+            return BadRequest(new { Message = "Erro interno no servidor" });
+        }
+    }
+}
+
