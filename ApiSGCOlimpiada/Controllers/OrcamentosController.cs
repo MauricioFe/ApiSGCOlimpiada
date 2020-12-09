e371b51ada1ceb4009cecb2470d03a62d93996a0
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-12-09 16:20:35 -0300

    Adicionando autorização aos controllers

--- ApiSGCOlimpiada/Controllers/OrcamentosController.cs
+++ ApiSGCOlimpiada/Controllers/OrcamentosController.cs
@@ -9,9 +9,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ApiSGCOlimpiada.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class OrcamentosController : ControllerBase
