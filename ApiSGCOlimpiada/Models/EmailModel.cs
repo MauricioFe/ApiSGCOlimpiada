349f3c236de533be131a4f793317c2cdd4b05f84
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-01-28 10:15:24 -0300

    Refatorando o model de email

--- ApiSGCOlimpiada/Models/EmailModel.cs
+++ ApiSGCOlimpiada/Models/EmailModel.cs
@@ -7,17 +7,12 @@ namespace ApiSGCOlimpiada.Models
 {
     public class EmailModel
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Ocupacao { get; set; }
-        public string Competidor { get; set; }
-        public string avaliador { get; set; }
+        public Responsavel Responsavel { get; set; }
+        public Orcamento Orcamento { get; set; }
+        public Acompanhamento Acompanhamento { get; set; }
         public string CodUnidadeOrganizacional { get; set; }
-        public string CentroResponsabilidade { get; set; }
+        public string CentroResponsabilidade{ get; set; }
         public string ClasseValor { get; set; }
-        public string Cnpj { get; set; }
         public string UnidadeOrganizacional { get; set; }
-        public string Orcamento { get; set; }
-        public string Valor { get; set; }
     }
 }
