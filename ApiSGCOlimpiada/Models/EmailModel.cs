3002a8af8fa89748194d61b40b7b0555c9e3b8fb
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-01-29 14:40:34 -0300

    Corrigindo erros

--- ApiSGCOlimpiada/Models/EmailModel.cs
+++ ApiSGCOlimpiada/Models/EmailModel.cs
@@ -11,7 +11,7 @@ namespace ApiSGCOlimpiada.Models
         public string CentroResponsabilidade { get; set; }
         public string ClasseValor { get; set; }
         public string UnidadeOrganizacional { get; set; }
-        public string ContaContábil { get; set; }
+        public string ContaContabil { get; set; }
         public List<Responsavel> Responsaveis { get; set; }
         public Orcamento Orcamento { get; set; }
         public List<Ocupacao> Ocupacoes { get; set; }
