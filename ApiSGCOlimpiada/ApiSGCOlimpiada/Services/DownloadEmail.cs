8ac884aabf5c9068032c9bdcceaec641ab82c954
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-03-09 14:36:58 -0300

    Refatoração de código

---
+++ ApiSGCOlimpiada/ApiSGCOlimpiada/Services/DownloadEmail.cs
@@ -0,0 +1,29 @@
+﻿using ApiSGCOlimpiada.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Coravel.Mailer.Mail;
+
+namespace ApiSGCOlimpiada.Services
+{
+    public class DownloadEmail : Mailable<EmailModel>
+    {
+        private EmailModel data;
+
+        public DownloadEmail(EmailModel data) => this.data = data;
+
+        public override void Build()
+        {
+            List<string> address = new List<string>();
+            foreach (var item in data.Responsaveis)
+            {
+                address.Add(item.Email);
+            }
+            this.To("mauricio.lacerdaml@gmail.com")
+                .From(new MailRecipient("olimpiada@gmail.com", "Envio da solicitação de compra"))
+                .Subject($"Realizando teste de envio de email")
+                .View("~/Views/testeEmail.cshtml", this.data);
+        }
+    }
+}
